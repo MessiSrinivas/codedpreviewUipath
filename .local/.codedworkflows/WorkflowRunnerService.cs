0e5417f4109ec294d658e89f3edb542cc9c6b642
Srinivas <srinivas.gitiga@testingxperts.com>
2024-01-30 18:57:41 +0530

    Deleted Unnecessary files

---
+++ .local/.codedworkflows/WorkflowRunnerService.cs
@@ -0,0 +1,41 @@
+using DecodeTest.ObjectRepository;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Excel;
+using UiPath.Excel.Activities;
+using UiPath.Excel.Activities.API;
+using UiPath.Excel.Activities.API.Models;
+using UiPath.Orchestrator.Client.Models;
+using UiPath.Testing;
+using UiPath.Testing.Activities.TestData;
+using UiPath.Testing.Activities.TestDataQueues.Enums;
+using UiPath.Testing.Enums;
+using UiPath.UIAutomationNext.API.Contracts;
+using UiPath.UIAutomationNext.API.Models;
+using UiPath.UIAutomationNext.Enums;
+using System.Threading.Tasks;
+using UiPath.Activities.Contracts;
+
+namespace DecodeTest
+{
+    public class WorkflowRunnerService
+    {
+        private readonly Func<string, IDictionary<string, object>, TimeSpan?, bool, InvokeTargetSession, IDictionary<string, object>> _runWorkflowHandler;
+        public WorkflowRunnerService(Func<string, IDictionary<string, object>, TimeSpan?, bool, InvokeTargetSession, IDictionary<string, object>> runWorkflowHandler)
+        {
+            _runWorkflowHandler = runWorkflowHandler;
+        }
+
+        /// <summary>
+        /// Invokes the AdactinLogIn.cs
+        /// </summary>
+        public void AdactinLogIn()
+        {
+            var result = _runWorkflowHandler(@"AdactinLogIn.cs", new Dictionary<string, object>{}, default, default, default);
+        }
+    }
+}
